eb0bcd59c16ae1564e00c88c6b264fae5e697345
hyuganatsu <amanatsu.mp@gmail.com>
2017-06-17 18:48:17 +0900

    Git2 sqlパッケージ追加　LoginToMenuを更新

--- workspace/TeMS/src/stp/LoginToMenu.java
+++ workspace/TeMS/src/stp/LoginToMenu.java
@@ -8,12 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import sql.SQLCommand;
+
 @WebServlet ("/stp/LoginToMenu")
 public class LoginToMenu extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("サーブレット");
+		String sql = SQLCommand.outputMonth;
+		System.out.println(sql + " を実行しました");
 		getServletConfig().getServletContext().
 		getRequestDispatcher("/menu.jsp" ).
 		forward( request, response );
